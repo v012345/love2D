c384072bc1a44a3ae1aea92b2b797cd20d085d54
v012345 <v012345@163.com>
2026-03-25 23:02:24 +0800

    小丑牌 lua

--- assets/conf.lua
+++ assets/conf.lua
@@ -1,8 +1,11 @@
--- conf.lua - LÖVE 配置文件（可选）
+_RELEASE_MODE = true
+_DEMO = false
 
 function love.conf(t)
-    t.window.title = "My First LÖVE Game"
-    t.window.width = 1920
-    t.window.height = 1080
-    t.window.resizable = true
-end
+	t.console = not _RELEASE_MODE
+	t.title = 'Balatro'
+	t.window.width = 0
+    t.window.height = 0
+	t.window.minwidth = 100
+	t.window.minheight = 100
+end 
